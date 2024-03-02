9c921fe792336e846935fe78186aa3fd93d68f6a
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2024-03-02 22:41:49 +0530

    created Add new class view

--- lms-frontend/src/pages/Admin/class/AdminNewClass.jsx
+++ lms-frontend/src/pages/Admin/class/AdminNewClass.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import CardNewAdding from '../../../components/CardNewAdding'
+import { Box, Grid } from '@mui/material'
+import ClassViewCard from '../../../components/ClassViewCard'
 
 function AdminNewClass() {
   return (
@@ -7,6 +9,19 @@ function AdminNewClass() {
       <CardNewAdding 
         name="New Class"
       />
+                      <Box sx={{ maxHeight: '400px', overflowY: 'auto' }}>
+                      <Grid container direction="row"  rowSpacing={2} columnSpacing={2}>
+                        <ClassViewCard subject="Commerce" grade="12" teacher="Jagath Chandhana"/>
+                        <ClassViewCard subject="Commerce" grade="12" teacher="Jagath Chandhana"/>
+                        <ClassViewCard subject="Commerce" grade="12" teacher="Jagath Chandhana"/>
+                        <ClassViewCard subject="Commerce" grade="12" teacher="Jagath Chandhana"/>
+                       
+                      </Grid>
+
+                      </Box>
+
+
+      
     </div>
   )
 }
