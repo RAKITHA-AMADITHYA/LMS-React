985856e72b88f7e48f0251196a9d8f1cfefaa35f
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2024-03-03 18:17:24 +0530

    made navigation between add class

--- lms-frontend/src/pages/Admin/class/AdminNewClass.jsx
+++ lms-frontend/src/pages/Admin/class/AdminNewClass.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import CardNewAdding from '../../../components/CardNewAdding'
-import { Box, Grid } from '@mui/material'
+import { Box, Grid,Button , TextField, Typography, Stack } from '@mui/material'
 import ClassViewCard from '../../../components/ClassViewCard'
+import { Link } from 'react-router-dom'
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import SecuritySharpIcon from '@mui/icons-material/SecuritySharp';
 
 function AdminNewClass() {
   return (
     <div className='h-screen'>
       <CardNewAdding 
         name="New Class"
+        path="/admin/new-class/grade-subject"
       />
                       <Box sx={{ maxHeight: '400px', overflowY: 'auto' }}>
                       <Grid container direction="row"  rowSpacing={2} columnSpacing={2}>
